0389db0bfa063bb69828e24d3cb86be328df63a1
Maddee Rubenson <mrubenson@flowwest.com>
2025-04-08 16:01:05 -0700

    adding HUC and NF boundaries

---
+++ data-raw/shapefile_data_processing.R
@@ -0,0 +1,68 @@
+# data processing of the NF and HUC layers
+
+library(sf)
+library(tidyverse)
+library(leaflet)
+
+
+# national forests --------------------------------------------------------
+
+nf_raw <- read_sf(here::here('data-raw', 'shapefiles', 'nf_boundaries', 'S_USA.NFSLandUnit.shp')) |>
+  janitor::clean_names() |>
+  filter(nfslandu_1 == "National Forest") |>
+  glimpse()
+
+unique(nf_bounds$nfslandu_2)
+
+names <- c('Klamath National Forest',
+           'Lassen National Forest',
+           'Mendocino National Forest',
+           'Shasta National Forest',
+           'Modoc National Forest',
+           'Six Rivers National Forest')
+
+nf_bounds <- nf_raw |>
+  filter(nfslandu_2 %in% names) |>
+  select(name = nfslandu_2, hq_location = hq_locatio, geometry) |>
+  mutate(name = ifelse(name == "Shasta National Forest", "Shasta-Trinity National Forest", name)) |>
+  st_transform("+proj=longlat +datum=WGS84") |>
+  glimpse()
+
+leaflet() |>
+  addTiles() |>
+  addPolygons(data = nf_bounds,
+              popup = ~name)
+
+saveRDS(nf_bounds, here::here('data', 'nf_boundaries.RDS'))
+
+
+# watersheds --------------------------------------------------------------
+# NOTE: large geodatabases for CV and Klamath are currently stored on egnyte and the following file paths will need
+# to be updated to accomodate a different computer
+#
+# NOTE: we chose to use HUC6 but could adapt to HUC8 if a smaller area is desired from client
+klamath_gdb <- '../../../Egnyte - flowwest/Shared/Active_Projects/116-01_Water_Beneficiary_Database/Analysis/data-raw/nhdplus/klamath/NHDPLUS_H_1801_HU4_GDB/NHDPLUS_H_1801_HU4_GDB.gdb/'
+st_layers(klamath_gdb)
+huc6_raw_klamath <- st_read(klamath_gdb, layer = "WBDHU6")
+
+cv_gdb <- '../../../Egnyte - flowwest/Shared/Active_Projects/116-01_Water_Beneficiary_Database/Analysis/data-raw/nhdplus/central_valley/NHDPLUS_H_1802_HU4_GPKG/NHDPLUS_H_1802_HU4_GPKG.gpkg'
+st_layers(cv_gdb)
+huc6_raw_cv <- st_read(cv_gdb, layer = "WBDHU6")
+
+huc6_klamath <- huc6_raw_klamath |>
+  janitor::clean_names() |>
+  select(name, huc6, geometry = Shape) |>
+  st_transform(crs = "+proj=longlat +datum=WGS84") |>
+  st_zm()
+
+huc6_cv <- huc6_raw_cv |>
+  janitor::clean_names() |>
+  select(name, huc6, geometry = Shape) |>
+  st_transform(crs = "+proj=longlat +datum=WGS84") |>
+  st_zm() |>
+  glimpse()
+
+huc6 <- bind_rows(huc6_cv,
+                  huc6_klamath)
+
+saveRDS(huc6, here::here("data", "watersheds.RDS"))
