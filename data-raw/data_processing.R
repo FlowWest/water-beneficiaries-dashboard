90e39a6cfc019d1d1f3cc6ad071539b3e906789a
Maddee Rubenson <mrubenson@flowwest.com>
2025-04-09 14:22:36 -0700

    adding some comments

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -10,15 +10,12 @@ hydropower <- readRDS('data-raw/hydropower_clean.RDS') |>
   select(beneficiary_type, entity_name, entity_address, quantity_metric, quantity_unit, national_forest_connection, latitude, longitude, geometry)
 
 
-
-
-# map datasets to NF and HUCs ---------------------------------------------
-
-
-
-
 # save dataset ------------------------------------------------------------
 
 all_datasets <- bind_rows(hydropower)
+
+
+# map datasets to NF and HUCs ---------------------------------------------
+# TODO: explore ways of clipping the data to the HUCs
 
 saveRDS(all_datasets, here::here("data", "all_datasets.RDS"))
