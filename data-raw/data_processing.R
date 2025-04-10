debacb003cff542527c9162e14f56a9691e26e54
Badhia <byuneskatz@flowwest.com>
2025-04-09 17:35:05 -0700

    process cvp data to match hydropower coordinate reference

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -9,10 +9,24 @@ library(sf)
 hydropower <- readRDS('data-raw/hydropower_clean.RDS') |>
   select(beneficiary_type, entity_name, entity_address, quantity_metric, quantity_unit, national_forest_connection, latitude, longitude, geometry)
 
+cvp <- readRDS(here::here("data", "cvp.RDS"))
+
+cvp_centroids <- st_centroid(cvp)
+cvp_coords <- st_coordinates(cvp_centroids)
+cvp$longitude <- cvp_coords[,1]
+cvp$latitude <- cvp_coords[,2]
+
+cvp <- cvp |>
+  st_set_crs(4326) |>              # Set the original CRS (if missing)
+  st_transform(3857) |>            # Now transform to EPSG:3857
+  select(beneficiary_type, entity_name, entity_address, quantity_metric, quantity_unit,
+         national_forest_connection, latitude, longitude, geometry) |>
+  glimpse()
+
 
 # save dataset ------------------------------------------------------------
 
-all_datasets <- bind_rows(hydropower)
+all_datasets <- bind_rows(hydropower, cvp)
 
 
 # map datasets to NF and HUCs ---------------------------------------------
